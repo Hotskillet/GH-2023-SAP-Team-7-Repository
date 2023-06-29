d5445833d9e827b55edd74818297e4c0717c3400
Melissa Gray <61853436+mag535@users.noreply.github.com>
2023-06-29 16:48:56 -0400

    piece prefab demostration

---
+++ puzzleTest/Assets/Scripts/pseudoCodeTest.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pseudoCodeTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    /*
+    1) Player mouse hovers over piece, then click, should be allowed to drag piece around.
+    2) when dropped, piece checks for any collisions
+    3) if collision found, checks side compatability
+    4) if true, label a hit
+    */
+}
