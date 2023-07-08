e98c77c2c23de77996b67360810bf7a1340e1f2e
Hotskillet <124469811+Hotskillet@users.noreply.github.com>
2023-07-08 02:23:24 -0700

    started on inv system, made mockup nicer

---
+++ puzzleTest/Assets/Scripts/inventoryClassDefiner.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inventoryClassDefiner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+//making a class becuase im like 50% sure thats the way we handle this
+
+public class Inventory {
+    public Inventory(string[] contents, float lockItem) {
+        
+    }
+}
--- puzzleTest/Assets/baos testing/temporaryCoolInteraction.cs
+++ puzzleTest/Assets/baos testing/temporaryCoolInteraction.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class temporaryCoolInteraction : MonoBehaviour
 {
     public GameObject thing;
+    public GameObject thingTwo;
+
     // Start is called before the first frame update
     void Start()
     {
         thing.transform.position = new Vector3(500, 500, 0);
+        thingTwo.transform.position = new Vector3(500, 500, 0);
     }
 
     // Update is called once per frame
@@ -16,6 +19,9 @@ public class temporaryCoolInteraction : MonoBehaviour
     {
         if (Input.GetKeyDown("e")) {
             thing.transform.position = new Vector3(4.6f, -3.1f, 0.0f);
+        }
+        if (Input.GetKeyDown("q")) {
+            thingTwo.transform.position = new Vector3(2.34f, -3.1f, 0.0f);
         }
     }
 }
