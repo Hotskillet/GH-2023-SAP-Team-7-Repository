f0401c4cb5a1a2e3e1b3990e39974fa836961601
Melissa Gray <61853436+mag535@users.noreply.github.com>
2023-07-17 22:38:51 -0400

    update to items

--- puzzleTest/Assets/Scripts/Interaction/Door.cs
+++ puzzleTest/Assets/Scripts/Interaction/Door.cs
@@ -2,15 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Door : Item
+// doors store the layout (which room to swap to) and the coordinates 
+// or swap to a different scene
+// please do this
+// actually i just remembered what rebecca said
+// USE ROOM PREFABS (WITH ITEMS )PLEASEEEEEEEE
+
+public class Door : Unlockable
 {
-    // doors store the layout (which room to swap to) and the coordinates 
-    // or swap to a different scene
-    // please do this
-    // actually i just remembered what rebecca said
-    // USE ROOM PREFABS (WITH ITEMS )PLEASEEEEEEEE
-    public override void interact() { 
-        
+    // the scene number of the room the door is supposed to link to
+    public int nextRoom;
+
+    public void Start() 
+    {
+        locked = true;
+    }
+
+    private void unlock(){
+        // call Unlockable.interact()
+        base.interact();
+    }    
+
+    // FIXME: remove since Player will be calling these functions
+    private void OnCollisionExit2D(Collision2D other) {
+        unlock();
     }
 
 
--- puzzleTest/Assets/Scripts/Interaction/Unlockable.cs
+++ puzzleTest/Assets/Scripts/Interaction/Unlockable.cs
@@ -8,72 +8,52 @@ using UnityEngine;
 public class Unlockable : Item
 {
     public bool locked;
-    // an item contained in this object will be a child
-    public GameObject treasurePrefab;
+
+    private bool foundKey;
 
     // Start is called before the first frame update
     void Start()
     {
         locked = true;
+        foundKey = false;
     }
 
-    private void unlock(){
-        SpriteRenderer thisSprite = gameObject.GetComponent<SpriteRenderer>();
-        Color currCol = thisSprite.color;
-        // decrease opacity by 50%
-        thisSprite.color = new Color(currCol.r, currCol.g, currCol.b, currCol.a * 0.1f);
-        // disable collider
-        Collider2D hitbox = gameObject.GetComponent<Collider2D>();
-        hitbox.enabled = false;
-        // show treasure
-        if (treasurePrefab != null){
-            GameObject go = Instantiate(treasurePrefab, gameObject.transform.position, Quaternion.identity);
-            //FIXME: find a way not to hard code this
-            go.name = "keyExample";
-        }
+    public bool GetFoundKey(){
+        return foundKey;
     }
+
 
     public override void interact()
     {
         /* Steps:
             1. Check if Player's inventory has item that matches an item in this item's list
             2. update state
-            3. enable treasure
             4. disable this object
         */
         // Step 1: Check if Player's inventory has item that matches an item in this item's list
         ItemData temp = ItemManager.Instance.GetData(gameObject.name);
         if (temp == null){
-            Debug.Log("Item not in database");
+            Debug.Log("Item not in database");  //FIXME: UI & Narrative
             return;
         }
         foreach (string possibleMatch in temp.canInteractWith){
             if (Inventory.Instance.findItemIndex(possibleMatch) != -1){
                 // Step 2: Update state
                 locked = false;
-                Debug.Log(possibleMatch + " opened " + gameObject.name);
+                foundKey = true;
+                // Steap 3: Disable this object's collider
+                Collider2D hitbox = gameObject.GetComponent<Collider2D>();
+                hitbox.enabled = false;
+                //FIXME: replace with animation & SFX
+                SpriteRenderer thisSprite = gameObject.GetComponent<SpriteRenderer>();
+                Color currCol = thisSprite.color;
+                thisSprite.color = new Color(currCol.r, currCol.g, currCol.b, currCol.a * 0.1f);
                 //FIXME: remove item from inventory?
                 Inventory.Instance.RemoveItem(possibleMatch);
-                // Step 3: Enable treasure
-                // Steap 4: Disable this object
-                //gameObject.SetActive(false);
-                unlock();
                 return;
             }
         }
-        Debug.Log("You don't have the correct item to open this.");
+        Debug.Log("You don't have the correct item to open this.");  //FIXME: UI & Narrative
         return;
-    }
-
-    /*
-    // FIXME: remove since Player will be calling these functions
-    private void OnCollisionExit2D(Collision2D other) {
-        interact();
-    }
-    */
-
-    // Update is called once per frame
-    void Update()
-    {
     }
 }
