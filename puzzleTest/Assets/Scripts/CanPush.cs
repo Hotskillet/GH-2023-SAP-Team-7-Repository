810a7c2221738d3548f3cee9c700a66e5b571efd
Hotskillet <124469811+Hotskillet@users.noreply.github.com>
2023-08-05 17:28:43 -0700

    made boxes semipushable

---
+++ puzzleTest/Assets/Scripts/CanPush.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanPush : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.tag == "Player") {
+            rb.bodyType = RigidbodyType2D.Dynamic;
+        } /*else if (collision.gameObject.tag != "Player") {
+            rb.bodyType = RigidbodyType2D.Static;
+
+        }
+        */
+    }
+
+    void OnCollisionExit2D(Collision2D collision) {
+            rb.bodyType = RigidbodyType2D.Static;
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+
+        rb = GetComponent<Rigidbody2D>();
+        rb.bodyType = RigidbodyType2D.Static;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
