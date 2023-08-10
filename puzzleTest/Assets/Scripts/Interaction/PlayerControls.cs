631c5c865c0dffdc13ccf7cda00325796764c8d5
Melissa Gray <61853436+mag535@users.noreply.github.com>
2023-08-10 16:48:43 -0400

    fixed item bug  #1
    Code for the dialogue manager was throwing errors (the code doesn't currently work)

--- puzzleTest/Assets/Scripts/Interaction/PlayerControls.cs
+++ puzzleTest/Assets/Scripts/Interaction/PlayerControls.cs
@@ -185,7 +185,6 @@ public class PlayerControls : MonoBehaviour
     }
     public void ChangeMap(ChangeInputMap evt){
         if (evt.map != playerInput.currentActionMap.name){
-            Debug.Log("changing map");
             playerInput.SwitchCurrentActionMap(evt.map);
         }
     }
@@ -206,7 +205,6 @@ public class PlayerControls : MonoBehaviour
 
     // Update
     private void Update(){
-        Debug.Log(playerInput.currentActionMap.name);
         // resets movementVector to (0,0,0)
         ResetMovement();
 
