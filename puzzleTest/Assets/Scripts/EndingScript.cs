e3a11f5abebb8e14f78a1e042c2fdafd3cbb771f
Dejon06 <124469772+Dejon06@users.noreply.github.com>
2023-11-13 13:01:35 -0800

    credits are fully black now

--- puzzleTest/Assets/Scripts/EndingScript.cs
+++ puzzleTest/Assets/Scripts/EndingScript.cs
@@ -15,6 +15,7 @@ IEnumerator DelayedReturn(float d){
   // wait for a bit so player can see "Thanks for playing!"
   yield return new WaitForSeconds(d);
   // go back to main menu
-  SceneLoader.Instance.GoToScene("MainMenuUI");
+  // SceneLoader.Instance.GoToScene("MainMenuUI"); old code in case it breaks - de'jon
+  SceneLoader.Instance.GoToScene("Credits Roll");
 }
 }
