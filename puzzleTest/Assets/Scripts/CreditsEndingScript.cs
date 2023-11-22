d95f651ea41d12cce8cf1b3e599c0891845fae16
Dejon06 <124469772+Dejon06@users.noreply.github.com>
2023-11-22 10:22:15 -0800

    code works but only on boot?
    it seems like everything works as normal when its the first scene, but after that it doesn't work at all??? you gotta love programming

--- puzzleTest/Assets/Scripts/CreditsEndingScript.cs
+++ puzzleTest/Assets/Scripts/CreditsEndingScript.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CreditsEndingScript : MonoBehaviour
 {
     public float delay;
     public AudioManager music;
+    public string scenename;
 
 void Start(){
   // this will run as soon as scene is loaded
@@ -16,9 +18,9 @@ IEnumerator DelayedReturn(float d){
   // wait for a bit so player can see "Thanks for playing!"
   yield return new WaitForSeconds(d);
   // go back to main menu
-  // SceneLoader.Instance.GoToScene("MainMenuUI"); old code in case it breaks - de'jon
+  // SceneLoader.Instance.GoToScene("MainMenuUI"); old code in case it breaks - de'jon - SceneLoader.Instance.GoToScene("Intro");
   music.Stop("CreditsMusic");
-  SceneLoader.Instance.GoToScene("Intro");
+  SceneManager.LoadScene(scenename);
 }
 }
 
